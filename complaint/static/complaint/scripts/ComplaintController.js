58277a7b237454c74d07d5734cb136b5b900a9c8
Karthikeyan K <karthipec@gmail.com>
2013-10-19 03:08:35 +0530

    resolving conflicts in complaint controller js

--- complaint/static/complaint/scripts/ComplaintController.js
+++ complaint/static/complaint/scripts/ComplaintController.js
@@ -4,26 +4,18 @@ app.controller("ComplaintController", function($scope, $http) {
 		date_entered : new Date()
 	};
 
-	$http.get('/api/v1/location/?format=json')
-		.success(function(data) {
-			$scope.locations = data.objects;
+	$http.get('/api/v1/location/?format=json').success(function(data) {
+		$scope.locations = data.objects;
 	});
-	
-	$http.get('/api/v1/category/?format=json')
-	.success(function(data) {
+
+	$http.get('/api/v1/category/?format=json').success(function(data) {
 		$scope.categories = data.objects;
 	});
-	
-	$http.get('/api/v1/location/?format=json')
-	.success(function(data) {
-		$scope.locations = data.objects;
-});
-	
-	$http.get('/accounts/info')
-		.success(function(data) {
-			$scope.complaint.user = data[0].resource_uri;
+
+	$http.get('/accounts/info').success(function(data) {
+		$scope.complaint.user = data[0].resource_uri;
 	});
-	
+
 	$scope.postComplaint = function() {
 
 		$http({
@@ -33,47 +25,64 @@ app.controller("ComplaintController", function($scope, $http) {
 			headers : {
 				'content-type' : 'application/json'
 			}
-		})
-		.success(function(data) {
+		}).success(function(data) {
 			$scope.cancel();
-		})
-		.error(function(data) {
-			
+		}).error(function(data) {
+
 		});
 	};
 });
 
 app.controller('MyComplaintsController', function($scope, $http) {
-	$http.get('/complaint/').success(function(data) {
-		$scope.complaints = data;
+	$http.get('/complaint/').success(
+			function(data) {
+				$scope.complaints = data;
 
-		if ($scope.complaints && $scope.complaints.length > 0)
-			for (var i = 0; i < $scope.complaints.length; i++) {
-				var complaint = $scope.complaints[i];
-				$http.get('/complaint/' + complaint.id + '/followers')
-					.success(function(followers) {
-						complaint.followers = followers;
-				});
-				$http.get(complaint.locality)
-					.success(function(locality) {
-						complaint.location = locality.name;
-				});
-			};
+				if ($scope.complaints && $scope.complaints.length > 0)
+					for ( var i = 0; i < $scope.complaints.length; i++) {
+						var complaint = $scope.complaints[i];
+						$http.get('/complaint/' + complaint.id + '/followers')
+								.success(function(followers) {
+									complaint.followers = followers;
+								});
+						$http.get(complaint.locality).success(
+								function(locality) {
+									complaint.location = locality.name;
+								});
+					}
+				;
+			});
+
+	$http.get('/complaint/localcomplaints/').success(function(data) {
+		$scope.localityComplaints = data;
+	}).error(function(data) {
+		$scope.localityComplaints = [];
 	});
-	
-	$http.get('/complaint/localcomplaints/')
-		.success(function(data) {
-			$scope.localityComplaints = data;
-		})
-		.error(function(data){
-			$scope.localityComplaints = [];
-		});
-	
+
 	$scope.upvote = function(complaint) {
-		$http.get('/complaint/upvote/'+complaint.id + '/')
-		.success(function(data) {
-			complaint.upvotes += 1;
-		});
+		$http.get('/complaint/upvote/' + complaint.id + '/').success(
+				function(data) {
+					complaint.upvotes += 1;
+				});
 	};
 });
 
+app.controller('AdminComplaintsController', function($scope) {
+	$http.get('/complaint/top-complaints/').success(function(data) {
+		$scope.topComplaints = data;
+	}).error(function(data) {
+		$scope.topComplaints = [];
+	});
+
+	$http.get('/complaint/recent-complaints/').success(function(data) {
+		$scope.recentComplaints = data;
+	}).error(function(data) {
+		$scope.recentComplaints = [];
+	});
+
+	$http.get('/complaint/all-complaints/').success(function(data) {
+		$scope.allComplaints = data;
+	}).error(function(data) {
+		$scope.allComplaints = [];
+	});
+});
