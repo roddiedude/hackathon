3deb3fa01bcb19f85f617b828396cad0aee1ca86
Karthikeyan K <karthipec@gmail.com>
2013-10-18 16:50:15 +0530

    Adding routing logic in landing page and also committing the partials

--- accounts/static/accounts/js/login.js
+++ accounts/static/accounts/js/login.js
@@ -1,3 +1,22 @@
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/home',
+            {
+                controller: 'HomeController',
+                templateUrl: '../partials/home.html'
+            })
+        .when('/signup',
+            {
+                controller: 'SignupController',
+                templateUrl: '../partials/sign-up.html'
+            })
+        .otherwise({ redirectTo: '/signup' });
+});
+
+app.controller('HomeController', function($scope) {
+	console.log('Home controller activated.');
+});
+
 app.controller('LoginController', function ($scope, $http, $location) {
 
     $scope.login = function () {
@@ -18,3 +37,27 @@ app.controller('LoginController', function ($scope, $http, $location) {
         });
     };
 });
+
+app.controller('SignupController', function($scope, $http, $location) {
+	
+    $scope.isSaveDisabled = function () {
+        return $scope.signUpForm.$invalid;
+    };
+	
+    $scope.signup = function () {
+        $http({
+            url: '/signup',
+            method: 'POST',
+            data: $scope.user,
+            headers: { 'content-type': 'application/json' }
+        }).success(function (data) {
+            if (data.message == 'success') {
+                document.location.href = '/landingPage';
+            }
+        }).error(function (data) {
+            console.log('error occurred while creating user.')
+        });
+    };
+});
+
+
